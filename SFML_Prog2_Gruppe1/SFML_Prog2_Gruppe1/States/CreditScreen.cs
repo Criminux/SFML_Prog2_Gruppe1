a12ecfe5f34cfd3e357a7564212ca5c4a04f8be8
MrThumbsUp <gebebi@gmail.com>
2016-11-06 17:39:48 +0100

    using directives added

--- SFML_Prog2_Gruppe1/SFML_Prog2_Gruppe1/States/CreditScreen.cs
+++ SFML_Prog2_Gruppe1/SFML_Prog2_Gruppe1/States/CreditScreen.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SFML;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.Audio;
+
 namespace SFML_Prog2_Gruppe1.States
 {
     public class CreditScreen : State

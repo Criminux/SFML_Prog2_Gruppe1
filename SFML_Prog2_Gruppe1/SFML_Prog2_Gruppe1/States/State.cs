aa9e3a0bc34c0ea492255e17efb9e283e7363865
MrThumbsUp <gebebi@gmail.com>
2016-11-06 17:22:43 +0100

    Implemented StateMachine
    Nice MayMay... much wow

---
+++ SFML_Prog2_Gruppe1/SFML_Prog2_Gruppe1/States/State.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFML_Prog2_Gruppe1
+{
+    public abstract class State
+    {
+        public abstract void Initialize();
+
+        public abstract GameStates Update();
+
+        public abstract void Draw();
+
+        public abstract void Dispose();
+    }
+}
